fb9b2b12ecceaf0c12ca0e45845cee18a277d8c9
Valentin-roma <valentin.roymamer@icloud.com>
2025-03-10 19:17:51 +0100

    Update Code Clignotant Bouton + Falling.py

--- Code Clignotant Bouton + Falling.py
+++ Code Clignotant Bouton + Falling.py
@@ -8,16 +8,6 @@ def setup():
     GPIO.setmode(GPIO.BOARD)#Définit le mode de numération des broches en mode BOARD (numérotation physique)
     GPIO.setup(led,GPIO.OUT)#Configure la broche dee la LED comme une sortie 
     GPIO.setup(bouton,GPIO.IN)#Configure la broche du bouton comme une entrée
-
-"""def main():
-    while True:
-        if GPIO.input(bouton)==GPIO.HIGH:
-            GPIO.output(led, GPIO.HIGH)
-            print("la led s'allume !")
-        else:
-            GPIO.output(led, GPIO.LOW)
-            print("la led s'éteint !")
-"""
         
 def my_callback(bouton):#Fonction de rappel executé lorsqu'un évènement est détecté
     """Callback exécuté lorsqu'un appui sur le bouton est détecté"""
@@ -29,31 +19,6 @@ def main():
         GPIO.output(led,GPIO.HIGH)#Allume la LED
 
 
-"""
-GPIO.setup(led, GPIO.OUT)
-GPIO.setup(bouton, GPIO.IN)
-
-valeur = GPIO.input(bouton)
-GPIO.output(led, 1)
-
-if GPIO.input(bouton):
-    print('Input was HIGH')
-
-else:
-    print('Input was LOW')
-
-while GPIO.input(bouton) == GPIO.LOW:
-    time.sleep(0.01)  # wait 10 ms to give CPU chance to do other things
-
-
-def my_callback(bouton):
-    print('This is a edge event callback function!')
-    print('Edge detected on channel %s'%channel)
-    print('This is run in a different thread to your main program')
-
-#add rising edge detection on a channel, ignoring further edges for 200ms for switch bounce handling
-GPIO.add_event_detect(channel, GPIO.RISING, callback=my_callback, bouncetime=200)
-"""
 
 def destroy():
     #Turn Off LED
