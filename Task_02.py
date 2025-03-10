cf1f645f1a1c8355f1cad550130c8cd42d0f72a9
Valentin-roma <valentin.roymamer@icloud.com>
2025-03-10 19:42:50 +0100

    Update Task_02.py

--- Task_02.py
+++ Task_02.py
@@ -11,9 +11,9 @@ def setup():
     GPIO.setup(led,GPIO.OUT)#Configure la broche dee la LED comme une sortie 
     GPIO.setup(bouton,GPIO.IN)#Configure la broche du bouton comme une entrée
 
-
+#Interruptions
 def main(): 
-    while True: # Vérifie l'état du bouton
+    while True: #boucle infinie
         if GPIO.input(bouton)==GPIO.LOW: #Si le bouton est préssé
             GPIO.output(led, GPIO.LOW) #Alors la LED s'allume (LOW pour qu'il y ait une différence de potentiel)
             print("la led s'allume !")
@@ -21,7 +21,19 @@ def main():
             GPIO.output(led, GPIO.HIGH) #Sinon la LED s'éteint (HIGH pour que la différence de potentiel soit nulle)
             print("la led s'éteint !")
 
+def my_callback(channel):
+    if GPIO.input(bouton) == GPIO.LOW:  # Si le bouton est pressé
+        GPIO.output(led, GPIO.LOW)  # Allume la LED
+        print("La LED s'allume !")
+    else:
+        GPIO.output(led, GPIO.HIGH)  # Éteint la LED
+        print("La LED s'éteint !")
 
+def main():
+    GPIO.add_event_detect(bouton, GPIO.BOTH, callback=my_callback)# Configurer une interruption sur le flanc descendant (appui sur le bouton)
+    try:
+        while True:  # Boucle principale (le programme peut faire autre chose)
+            time.sleep(1)  # Réduire l'utilisation du CPU
 
 
 
