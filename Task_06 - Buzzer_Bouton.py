ab784946645e7f7876f49524531b6bf662cbd622
Valentin-roma <valentin.roymamer@icloud.com>
2025-05-04 15:33:56 +0200

    Update Task_06 - Buzzer_Bouton.py

--- Task_06 - Buzzer_Bouton.py
+++ Task_06 - Buzzer_Bouton.py
@@ -1,11 +1,9 @@
 
 import RPi.GPIO as GPIO    #importe la bibliotheque RPIO.GPIO pour controler la carte
-from kivy.app import App
-from kivy.uix.button import Button
 import time    #importe la bibliotheque time
 buzzer=11                                                                                   
 bouton=12
-etat_led=False    #Etat de la LED
+etat_buzz=False    #Etat de la LED
 def setup():
     GPIO.setmode(GPIO.BOARD)    #Définit le mode de numération des broches en mode  physique)
     GPIO.setup(buzzer,GPIO.OUT)    #Configure la broche dee la LED comme une sortie 
@@ -13,20 +11,20 @@ def setup():
    
 
 def verification(x):
-    global etat_led
+    global etat_buzz #definti une vaariable global
     if GPIO.input(x) == GPIO.LOW:    # Si le bouton est pressé (LOW en mode pull-up)
-        etat_led = not etat_led    # Inverse l'état des leds selon l'appui du bouton
+        etat_buzz = not etat_buzz    # Inverse l'état des leds selon l'appui du bouton
 
 def main():
-    GPIO.add_event_detect(bouton,GPIO.BOTH,callback=verification)    # Attend l'événement de l'appui du bouton pour appeler la fonction verification au dessus
+    GPIO.add_event_detect(bouton,GPIO.BOTH,callback=verification,bouncetime=300)    # Attend l'événement de l'appui du bouton pour appeler la fonction verification au dessus
     GPIO.output(buzzer,GPIO.LOW)
     while True:    #définit une condition toujours vraie
         if verification:    # Si le bouton est pressé
-            GPIO.output(buzzer,GPIO.HIGH)    # Allume la LED
-            time.sleep(0.1)    # Attendre h1 seconde
-            GPIO.output(buzzer,GPIO.LOW)    # Éteint la LE 
-            time.sleep(0.1)
-        time.sleep(0.5)
+            GPIO.output(buzzer,GPIO.HIGH)    # Allume le buzzer
+            time.sleep(0.1)    # Attendre 0.1 seconde
+            GPIO.output(buzzer,GPIO.LOW)    # Éteint le buzzer
+            time.sleep(0.1) #attend 0.1 
+        time.sleep(0.5) #attend 0;5
 
 def destroy():
     #Turn Off LED
